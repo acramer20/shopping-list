2d039f1187cddb4fbe93464e6b8daeab37ef0f46
acramer20 <97561089+acramer20@users.noreply.github.com>
2022-07-04 13:21:05 -0700

    adding all

--- routes/items.test.js
+++ routes/items.test.js
@@ -20,13 +20,13 @@ afterEach(async () => {
 // GET /items - this should render a list of shopping items.
 
 describe("GET /items", async function () {
-    test("Gets a list of items", async function () {
-      const response = await request(app).get(`/items`);
-      const { items } = response.body;
-      expect(response.statusCode).toBe(200);
-      expect(items).toHaveLength(1);
-    });
+  test("Gets a list of items", async function () {
+    const response = await request(app).get(`/items`);
+    const { items } = response.body;
+    expect(response.statusCode).toBe(200);
+    expect(items).toHaveLength(1);
   });
+});
 
 //POST /items - this route should accept JSON data and add it to the shopping list.
 
